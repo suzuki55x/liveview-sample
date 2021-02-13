ace967c02650261711c087c290f831acab5cef19
suzuki_ta <suzuki_ta@alco-ex.jp>
2021-02-13 21:06:40 +0900

    Add postgreSQL DEV user

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :liveview_sample, LiveviewSample.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "dev_user",
+  password: "hogefuga",
   database: "liveview_sample_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
